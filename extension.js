9148cbf0979f9ecc28ed88dba735de91ed41d6e5
Major Péter <majorpeter29@gmail.com>
2017-11-20 22:41:31 +0100

    added dropdown menu and toggle feature

--- extension.js
+++ extension.js
@@ -5,6 +5,7 @@ const Soup = imports.gi.Soup;
 const Lang = imports.lang;
 const Clutter = imports.gi.Clutter;
 const PanelMenu = imports.ui.panelMenu;
+const PopupMenu = imports.ui.popupMenu;
 
 function debug_log(text) {
     log('[PwmClient-DEBUG] ' + text);
@@ -16,15 +17,21 @@ const PwmClient = new Lang.Class({
     Name: 'PwmClient',
     Extends: PanelMenu.Button,
 
+    _lightEnabled: false,
+
     _init: function () {
         this.parent(0.0, "PwmClient", false);
 
         this.buttonText = new St.Label({
-            text: 'text ',
+            text: 'Lighting',
             y_align: Clutter.ActorAlign.CENTER
         });
 
         this.actor.add_actor(this.buttonText);
+
+        let toggleMenuItem = new PopupMenu.PopupMenuItem(_('Toggle'));
+        toggleMenuItem.connect('activate', Lang.bind(this, this._toggle));
+        this.menu.addMenuItem(toggleMenuItem);
         
         this._loadStatus();
     },
@@ -32,18 +39,40 @@ const PwmClient = new Lang.Class({
     _loadStatus: function() {
         debug_log('Loading Status');
         
-        let params = {
-            cmd: 'status',
-        };
         var _httpSession = new Soup.Session()
-        let message =   Soup.form_request_new_from_hash('POST', RPC_URL, params);
+        let message =   Soup.form_request_new_from_hash('POST', RPC_URL, {
+            cmd: 'status',
+        });
         _httpSession.queue_message(message, Lang.bind(this, function (_httpSession, message) {
-                debug_log('HTTP Status: ' + message.status_code);
                 if (message.status_code !== 200) {
+                    debug_log('HTTP Error: ' + message.status_code);
                     return;
                 }
                 let json = JSON.parse(message.response_body.data);
                 debug_log('HTTP data: ' + JSON.stringify(json));
+
+                this._lightEnabled = (json.status == 'on');
+            }
+          )
+        );
+    },
+
+    _toggle: function() {
+        let command = 'default';
+        if (this._lightEnabled) {
+            command = 'off';
+        }
+        debug_log('Toggle (cmd = ' + command + ')');
+        var _httpSession = new Soup.Session()
+        let message =   Soup.form_request_new_from_hash('POST', RPC_URL, {
+            cmd: command,
+        });
+        _httpSession.queue_message(message, Lang.bind(this, function (_httpSession, message) {
+                if (message.status_code !== 200) {
+                    debug_log('HTTP Error: ' + message.status_code);
+                    return;
+                }
+                this._lightEnabled = !this._lightEnabled;
             }
           )
         );

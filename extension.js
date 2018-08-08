434b14467039ea771e616717461cec4d73c4e891
Major Péter <majorpeter29@gmail.com>
2018-08-08 22:22:14 +0200

    removed currently unsupported brightness control

--- extension.js
+++ extension.js
@@ -22,7 +22,6 @@ const PwmClient = new Lang.Class({
     _sliderR: null,
     _sliderG: null,
     _sliderB: null,
-    _sliderBr: null,
     _toggleAnimationTime: 0.5,
     _lastOnState: {'r': 0, 'g': 0, 'b': 0},
 
@@ -39,12 +38,10 @@ const PwmClient = new Lang.Class({
         this._sliderR = new SliderWithIcon('', 'background:red', 0);
         this._sliderG = new SliderWithIcon('', 'background:green', 0);
         this._sliderB = new SliderWithIcon('', 'background:blue', 0);
-        this._sliderBr = new SliderWithIcon('B', 'background:#f0f0f0;color:#333;', 1);
 
         this._sliderR._connectOnChange(Lang.bind(this, this._setColorFromSliders));
         this._sliderG._connectOnChange(Lang.bind(this, this._setColorFromSliders));
         this._sliderB._connectOnChange(Lang.bind(this, this._setColorFromSliders));
-        this._sliderBr._connectOnChange(Lang.bind(this, this._setBrightnessFromSlider));
 
         this._sliderR._connectIconClick(Lang.bind(this, this._sliderIconClicked, this._sliderR));
         this._sliderG._connectIconClick(Lang.bind(this, this._sliderIconClicked, this._sliderG));
@@ -53,7 +50,6 @@ const PwmClient = new Lang.Class({
         this.menu.addMenuItem(this._sliderR);
         this.menu.addMenuItem(this._sliderG);
         this.menu.addMenuItem(this._sliderB);
-        this.menu.addMenuItem(this._sliderBr);
 
         let toggleMenuItem = new PopupMenu.PopupMenuItem(_('Toggle'));
         toggleMenuItem.connect('activate', Lang.bind(this, this._toggle));
@@ -91,15 +87,7 @@ const PwmClient = new Lang.Class({
     },
 
     _onScrollEvent: function(actor, event) {
-        const SCROLL_BRIGHTNESS_INCREMENT = 0.03;
-        switch (event.get_scroll_direction()) {
-        case Clutter.ScrollDirection.UP:
-            this._setBrightnessAddDifference(SCROLL_BRIGHTNESS_INCREMENT);
-            break;
-        case Clutter.ScrollDirection.DOWN:
-            this._setBrightnessAddDifference(-SCROLL_BRIGHTNESS_INCREMENT);
-            break;
-        }
+        debug_log('Not implemented!')
     },
 
     _loadStatus: function() {
@@ -152,36 +140,6 @@ const PwmClient = new Lang.Class({
             }
           )
         );
-    },
-
-    _setBrightnessFromSlider: function() {
-        let brightness = Math.floor(this._sliderBr._getValue() * 100);
-        debug_log('Setting brightness: ' + brightness);
-
-        var _httpSession = new Soup.Session();
-        let message = Soup.form_request_new_from_hash('POST', RPC_URL, {
-            cmd: 'setbrightness',
-            b: String(brightness),
-        });
-        _httpSession.queue_message(message, Lang.bind(this, function (_httpSession, message) {
-                if (message.status_code !== 200) {
-                    debug_log('HTTP Error: ' + message.status_code);
-                }
-            }
-          )
-        );
-    },
-
-    _setBrightnessAddDifference: function(difference) {
-        let b = this._sliderBr._getValue();
-        b += difference;
-        if (b < 0) {
-            b = 0;
-        } else if (b > 1) {
-            b = 1;
-        }
-        this._sliderBr._setValue(b);
-        this._setBrightnessFromSlider();
     },
 
     _sliderIconClicked: function(event, arg1, slider) {
